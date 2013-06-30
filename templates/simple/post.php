4cb8e073f83e3f0108cd31ccbe9a5058d2e99c4f
Patrick LEFEVRE <patrick.lefevre@gmail.com>
2013-06-30 15:49:27 +0200

    Update post.php
    Support for images.

--- templates/simple/post.php
+++ templates/simple/post.php
@@ -15,6 +15,20 @@
 
         <div class="three-quarters post">
             <?php echo $post ?>
+            
+            <?php if (file_exists($image)) : ?>
+            <div style="margin-top:2em">
+                <figure>
+                    <img src="<?php echo $post_image ?>" alt="<?php echo $post_title ?>" />
+                </figure>
+            </div>
+            <?php elseif($pic): ?>
+            <div style="margin-top:2em">
+                <figure>
+                    <img src="<?php echo POSTS_DIR.$pic ?>" alt="<?php echo $post_title ?>" />
+                </figure>
+            </div>
+            <?php endif ?>
 
             <ul class="actions">
                 <li><a class="button" href="https://twitter.com/intent/tweet?screen_name=<?php echo $post_author_twitter ?>&text=Re:%20<?php echo $post_link ?>%20" data-dnt="true">Comment on Twitter</a></li>
